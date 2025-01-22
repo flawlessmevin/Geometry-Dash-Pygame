9404d7d6df8cae15abb25a055923c13c33cf6fbe
flawlessmevin <vanyaalisov10@gmail.com>
2025-01-22 15:58:52 +0100

    win menu

--- Game.py
+++ Game.py
@@ -106,6 +106,7 @@ class Game:
     def restart_game(self):
         pygame.mixer.music.stop()
         pygame.mixer.music.play(-1)
+        self.win_sound.stop()
 
         self.player = Player(100, 450)
         self.level = Level("assets/levels.txt")
@@ -114,9 +115,58 @@ class Game:
 
 
 
+
+
     def win_game(self):
         self.game_won = True
         pygame.mixer.music.stop()
         self.win_sound.play()
+
+        overlay = pygame.Surface((self.screen_width, self.screen_height), pygame.SRCALPHA)
+
+        font = pygame.font.Font("assets/fonts/ARCADECLASSIC.TTF", 72)
+        text = font.render("YOU WIN", True, (255, 255, 255))
+        text_rect = text.get_rect(center=(self.screen_width // 2, self.screen_height // 3))
+
+
+        button_font = pygame.font.Font("assets/fonts/ARCADECLASSIC.TTF", 36)
+        restart_button = self.create_buttons((self.screen_width // 2 - 150, self.screen_height // 2), "Restart",
+                                            button_font)
+        exit_button = self.create_buttons((self.screen_width // 2 - 150, self.screen_height // 2 + 100), "Exit",
+                                         button_font)
+
+
+        while True:
+            self.screen.blit(overlay, (0, 0))
+            self.screen.blit(text, text_rect)
+            self.screen.blit(*restart_button)
+            self.screen.blit(*exit_button)
+
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if restart_button[1].collidepoint(event.pos):
+                        self.restart_game()
+                        return
+                    if exit_button[1].collidepoint(event.pos):
+                        pygame.quit()
+                        exit()
+
+    def create_buttons(self, position, text, font):
+        button_surface = pygame.Surface((300, 50))
+        button_surface.fill((75, 75, 75))
+
+        text_surface = font.render(text, True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(150, 25))
+        button_surface.blit(text_surface, text_rect)
+
+        button_rect = button_surface.get_rect(topleft=position)
+
+        return button_surface, button_rect
+
 
 
