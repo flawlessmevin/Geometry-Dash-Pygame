8c12dcffea801e1ab4f14caa683f89dde9270b7c
flawlessmevin <vanyaalisov10@gmail.com>
2025-01-31 01:15:42 +0100

    settings from txt

--- Settings.py
+++ Settings.py
@@ -1,12 +1,29 @@
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-TILE_SIZE = 50
-
-LEVEL_LENGTH = 30
+import os
 
 
-FONT = "assets/fonts/ARCADECLASSIC.TTF"
+SETTINGS_FILE = "Settings.txt"
+
+
+settings = {}
+with open(SETTINGS_FILE, "r") as file:
+    for line in file:
+        if "=" in line:
+            key, value = line.strip().split(" = ", 1)
+            settings[key] = value
+
+
+
+
+SCREEN_WIDTH = int(settings.get("SCREEN_WIDTH", 800))
+SCREEN_HEIGHT = int(settings.get("SCREEN_HEIGHT", 600))
+TILE_SIZE = int(settings.get("TILE_SIZE", 50))
+
+LEVEL_LENGTH = int(settings.get("LEVEL_LENGTH", 20))
+
+
+
+
+FONT = settings.get("FONT", "assets/fonts/ARCADECLASSIC.TTF")
 
 PLAYER_IMAGES = ["assets/images/sprite.png",
                  "assets/images/sprite_jumping.png"
